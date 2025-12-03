1cb1b92d6de51db87c9f1754d3320a9a914f7211
alexlim7 <123590893+alexlim7@users.noreply.github.com>
2025-12-03 12:06:15 -0500

    Refactor factorial implementation and add main function

--- benchmarks/alexlim7-test3.lisp
+++ benchmarks/alexlim7-test3.lisp
@@ -1,12 +1,19 @@
-(define (mul-acc acc n)
-  (* acc n))
+(define (combine a b)
+  (let ((tmp (+ a b)))
+    (let ((a tmp))
+      (let ((b (- tmp 1)))
+        (- a b)))))
 
-(define (fact-iter n acc)
-  (if (= n 0)
-      acc
-      (fact-iter (sub1 n) (mul-acc acc n))))
+(define (wrap x y)
+  (let ((a (+ x 0)))
+    (let ((b (+ y 0)))
+      (combine a b))))
 
-(define (fact n)
-  (fact-iter n 1))
+(define (main)
+  (let ((x 10))
+    (let ((y 20))
+      (let ((r1 (wrap x y)))
+        (let ((r2 (wrap (+ x 1) (+ y 2))))
+          (+ r1 r2))))))
 
-(print (fact 7))
+(print (main))
